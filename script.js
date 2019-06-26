9f45abe922792300c45d789312da9960dad88720
EliseChia <swedesurrender@gmail.com>
2019-06-26 10:15:53 +0800

    First upload, everything working except easter egg

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-console.log("hello script js");
-
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+//Function to generate secret number
+var setSecretNumber = function () {
+return Math.floor(Math.random() * 11);
 };
+
+//save secret number to variable
+var secretNumber = setSecretNumber();
+console.log(secretNumber);
+
+var easterEgg = "help leh";
+
+//Create empty arrays to hold total, wins, losses
+var totalPlay = [];
+var wins = [];
+var losses = [];
+
+//assign variables to necessary DOM elements
+var inputBox = document.getElementById("guess");
+var resultDisplay = document.getElementById("result");
+var playTotal = document.getElementById("playtotal");
+var winsTotal = document.getElementById("wins");
+var lossTotal = document.getElementById("losses");
+
+//Function that runs when submit button is clicked
+var clicked = function () {
+    var userGuess = inputBox.value;
+    var parsedGuess = parseInt(userGuess);
+    if (parsedGuess === easterEgg) {
+        resultDisplay.textContent = "cannot leh";
+    } else if (parsedGuess === secretNumber) {
+        resultDisplay.textContent = "You got it!";
+        wins.push(1);
+        winsTotal.innerHTML = wins.length;
+    } else {
+        resultDisplay.textContent = "Sorry, try again..";
+        losses.push(1);
+        lossTotal.innerHTML = losses.length;
+    }
+    totalPlay.push(1);
+    playTotal.innerHTML = totalPlay.length;
+    secretNumber = setSecretNumber();
+    console.log(secretNumber);
+}
+
+//Event listener for button
+document.getElementById("submit").addEventListener("click", clicked);
